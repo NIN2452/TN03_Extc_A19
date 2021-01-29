b4084efac86bd9a0be35c3107440c230c00831e6
Nin2452 <ninadyadav2000@gmail.com>
2021-01-29 16:40:51 +0530

    sep css

---
+++ wbste/index.js
@@ -0,0 +1,49 @@
+ function validate()
+        {
+            if(document.getElementById('username').value=="Ninad")
+            {   if(document.getElementById('password').value=="12345")
+                {
+                    window.location.replace("https://nin2452.github.io/TN03_Extc_A19/wbste/1.html");
+                }
+                else
+                {
+                    alert("Naaay")
+                }
+            }
+            else if(document.getElementById('username').value=="Tanmay")
+            {   if(document.getElementById('password').value=="12345")
+                {
+                     window.location.replace("https://nin2452.github.io/TN03_Extc_A19/wbste/1.html");    
+                }
+                else
+                {
+                    alert("Naaay")
+                }
+            }
+            else if(document.getElementById('username').value=="Varun")
+            {   if(document.getElementById('password').value=="12345")
+                {
+                     window.location.replace("https://nin2452.github.io/TN03_Extc_A19/wbste/1.html");
+                }
+                else
+                {
+                    alert("Naaay")
+                }
+            }
+            else if(document.getElementById('username').value=="Atharva")
+            {   if(document.getElementById('password').value=="12345")
+                {
+                     window.location.replace("https://nin2452.github.io/TN03_Extc_A19/wbste/1.html");
+                }
+                else
+                {
+                    alert("Naaay")
+                }
+            }
+	        else{alert("nay")} 
+
+        }
+    function goto()
+    {
+        window.location.replace("https://nin2452.github.io/TN03_Extc_A19/wbste/registration.html");
+    }
