845a4c1bbdca738e2fd901194529aa7391a873de
NIN2452 <70601250+NIN2452@users.noreply.github.com>
2021-04-13 19:41:43 +0530

    finalizing 1

--- wbste/ehrsys-master/app.py
+++ wbste/ehrsys-master/app.py
@@ -1,6 +1,26 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect,g,request, url_for, session, render_template
+#from flask_sqlalchemy import SQLAlchemy
+from flask_mysqldb import MySQL
+import os
 
+# initialization of flask
 app = Flask(__name__)
+
+# for using flak-session
+app.secret_key = os.urandom(24)
+
+# adding encryption key
+app.secret_key = os.urandom(24)
+
+# comfiguring database values
+app.config["MYSQL_HOST"] = "localhost"
+app.config["MYSQL_USER"] = "newuser"
+app.config["MYSQL_PASSWORD"] = "Ninad2452"
+app.config["MYSQL_DB"] = "ehrsystem"
+
+# initializing database as db
+db = MySQL(app)
+
 
 @app.route("/")
 def home():
@@ -32,14 +52,68 @@ def services_pat():
 	return render_template("services_pat.html")
 
 
-@app.route("/login")
+# login page
+@app.route("/login", methods=["POST", "GET"])
 def login():
+	if request.method == "POST":
+		session.pop("user", None)
+		uid = request.form["userid"]
+		pswd = request.form["pass"]
+		table = request.form["table"]
+		cur = db.connection.cursor()
+		query = f"select * from login_{table} where user_id={uid}"
+		cur.execute(query)
+		usernamess = cur.fetchall()
+		try:
+			if usernamess[0][1] == pswd:
+				session["user"] = request.form["userid"]
+				return redirect(url_for("redirecting", table=table))
+			else:
+				return "password incorrect"
+		except Exception as e:
+				return("invalid username",e)
 	return render_template("login.html")
 
 
+# redirecting after logging in
+@app.route("/redirecting/<table>")
+def redirecting(table):
+    if table == "patient":
+        return redirect("/pat_home")
+    elif table == "doctor":
+        return redirect("/doc_home")
+    elif table == "receptionist":
+        return redirect("/rep_home.html")
+    elif table == "pathologist":
+        return redirect("/path_home.html")
+    else:
+        return "please login"
+
+
+# Sign Up
 @app.route("/signup")
 def signup():
-	return render_template("signup.html")
+    return render_template("signup.html")
+
+# checking and initialising user
+@app.before_request
+def before_request():
+    g.user = None
+
+    if "user" in session:
+        g.user = session["user"]
+
+
+# logout
+@app.route("/logout")
+def logout():
+    if g.user:
+        session.pop("user", None)
+        return redirect(url_for("login"))
+    return "you are already logged out"
+
+
+# -------------------------------------------------------------------------------------------------------------------------
 
 @app.route("/signup_doc")
 def signup_doc():
@@ -55,13 +129,31 @@ def signup_recep():
 
 # DOCTOR
 
+# Doctor's home
 @app.route("/doc_home")
 def doc_home():
-	return render_template("doc_home.html")
+    if g.user:
+        curser = db.connection.cursor()
+        query = f"select * from doctor where dr_id={g.user}"
+        result = curser.execute(query)
+        userdetail = curser.fetchall()
+        return render_template("doc_home.html", userinfo=userdetail)
+    return "please login first"
 
+# Doctor Profile
 @app.route("/doc_prof")
 def doc_prof():
-	return render_template("doc_prof.html")
+    if g.user:
+        curser = db.connection.cursor()
+        query = f"select * from doctor where dr_id={g.user}"
+        result = curser.execute(query)
+        userdetail = curser.fetchall()
+        return render_template("doc_prof.html", userinfo=userdetail)
+    return "please login first"
+
+
+# -------------------------------------------------------------------------------------------------------------------------
+
 
 @app.route("/doc_addpat")
 def doc_addpat():
